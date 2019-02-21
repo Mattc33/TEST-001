1444dcaa42d04007c5070fe68df28925ebe2d4ba
Hunter Pankey <hunterp@slalom.com>
2019-02-21 12:12:22 -0500

    Add credential dialog and parameter Add plain file deployment in uploadFiles Change output functions to write status on the left side instead of after the message

---
+++ provisioning/modules/uploadFiles.psm1
@@ -0,0 +1,40 @@
+function Start-Files {
+    [CmdletBinding()]
+    param(
+    [string] $DeployFolder,
+    [string] $File,
+    [object] $Step,
+    [bool]   $Cleanup,
+    [string] $Arg1
+    )
+  
+    $fullPath = Join-Path -Path $DeployFolder -ChildPath $File
+    Write-Info "Using CSV file from $fullPath"
+  
+    $lines = Import-Csv $fullPath
+  
+    foreach ($line in $lines) {
+        [boolean]$shouldUploadFile = $line.Execute
+        [string]$localFilePath = $line.LocalPathAndFilename
+        [string]$folderPath = $line.RemoteFolderSiteRelativePath
+        
+        if ($cleanup -eq $true) {
+        Write-Info "Cleanup not implemented ..."
+        return
+        }
+        
+        if($shouldUploadFile) {
+            Write-Info "Creating new page: $localFilePath"
+            if ([string]::IsNullOrEmpty($folderPath)) {
+            #   Add-PnPFile -localFilePath $localFilePath 
+                Write-Warning "$localFilePath does not have a designated folder. Skipping."
+            }
+            else {
+            Add-PnPFile -Path $localFilePath -Folder $folderPath | Out-Null
+            Write-Success "$localFilePath deployed to $folderPath."
+            }
+        } else {
+            Write-Info "$localFilePath marked as skip in deployment manifest CSV."
+        }
+    }
+}
