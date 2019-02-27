8ce76f1e9171e1314c33d819d81ac5b9f900f21f
Hunter Pankey <hunterp@slalom.com>
2019-02-27 12:00:09 -0500

    Initial checkin of data marketplace renderer

---
+++ src/extensions/dataMarketplaceRenderer/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IDataMarketplaceRendererApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'DataMarketplaceRendererApplicationCustomizerStrings' {
+  const strings: IDataMarketplaceRendererApplicationCustomizerStrings;
+  export = strings;
+}
