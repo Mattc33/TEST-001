fb720445bc4f4a9e4da29450f643d865bac272b1
javed.ansari <javeda@slalom.com>
2019-04-08 22:53:43 -0400

    report refactor, share, feedback & favorite

--- src/webparts/controls/index.ts
+++ src/webparts/controls/index.ts
@@ -3,7 +3,4 @@ export * from "./Toolbar/Toolbar";
 export * from "./ReportDiscussion/ReportDiscussionDialog";
 
 export * from "./ReportRenderers/TableauReport/TableauReport";
-export * from "./ReportRenderers/OfficeReport/OfficeReport";
-export * from "./ReportRenderers/PDFReport/PDFReport";
-export * from "./ReportRenderers/Others/ImageRenderer";
-export * from "./ReportRenderers/Others/UnknownRenderer";
+export * from "./ReportRenderers/GenericReport/GenericReport";
