9fedebb46eb4ad6a0c20207996c1ba22d84ed1ec
javed.ansari <javeda@slalom.com>
2019-04-10 00:33:24 -0400

    fixes report width, tableau JS load, favorite view

--- src/services/interfaces/IReportViewerService.ts
+++ src/services/interfaces/IReportViewerService.ts
@@ -1,6 +1,7 @@
-import { IReportItem } from "../../models";
+import { IReportItem, IFavoriteReport } from "../../models";
 
 export interface IReportViewerService {
     loadReportDefinition(reportId: number): Promise<IReportItem>;
+    loadFavorite(favoriteId: number): Promise<IFavoriteReport>;
     loadReportDefinitionByUrl(reportUrl: string, reportItem: IReportItem): Promise<IReportItem>;
 }
