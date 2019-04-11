f5b8768fd28225f586d4802f3980643bb8c480de
Sanjiv Sharma <sanjivs@slalom.com>
2019-04-11 08:38:48 -0400

    Fav Control Refactor in Progress

--- src/webparts/reportMyFav/components/MyFavHome.tsx
+++ src/webparts/reportMyFav/components/MyFavHome.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import styles from './MyFavHome.module.scss';
 import { IReportFavoriteItem } from "../../../models/IReportItem";
-import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
-import { ReportFavoriteType } from "../../../helpers/UrlHelper";
+import { IconButton } from 'office-ui-fabric-react/lib/Button';
+
 
 export interface IReportProps {
   key: string;
@@ -21,47 +21,39 @@ export default class MyFavHome extends React.Component<IReportProps, {}> {
   public render(): React.ReactElement<IReportProps> {
     const rowStyle = {
       display: 'inline-flex',
+      margin: '1px 1px',
+      width: '100%',
     };
 
     const colStyle = {
-      background: '#EEF0F2',
+      width: '80%',
     };
 
     console.log("reportItem: ", this.props.reportItem);
     let reportTitle = this.props.reportItem.Title;
     const reportDesc = this.props.reportItem.SVPVisualizationDescription;
-    if(this.props.reportItem.SVPFavoriteType != ReportFavoriteType.Original) {
-      reportTitle = this.props.reportItem.Title;
-    }
-
-
-    let reportImageUrl = this.props.reportItem.SVPVisualizationImage;
-    if(this.props.reportItem.SVPFavoriteType != ReportFavoriteType.Original) {
-      //TODO: Get the URL from SVPVisualizationMetadata:
-      reportImageUrl = "#";
-    }
 
     return (
       <div className={styles.MyFavHome}>
         <div className={styles.wrapper}>
           <div className="row" style={rowStyle}>
-            <div className="col-md-6">
-            <p className={styles.title}>{reportTitle}</p>
+            <div className="col-sm-8" style={colStyle}>
+              <p className={styles.title}>{reportTitle}</p>
             </div>
-            <div className="col-md-6" >
+            <div className="col-sm-4" >
                 <p>
-                <PrimaryButton data-automation-id="favReportView" text="View" className={styles.button}
-                  onClick={(e) => this.props.onView(this.props.reportItem)} />
-                <PrimaryButton data-automation-id="favReportView" text="Share" className={styles.button}
-                  onClick={(e) => this.props.onShare(this.props.reportItem)} />
-                <PrimaryButton data-automation-id="favReportRemove" text="Remove" className={styles.button}
-                  onClick={(e) => this.props.onRemove(this.props.reportItem)} />
-                  </p>
+                  <IconButton iconProps={{ iconName: 'AreaChart' }} title="View Report" ariaLabel="View Report" onClick={(e) => this.props.onView(this.props.reportItem)}/>
+                  <IconButton iconProps={{ iconName: 'Share' }} title="Share Report" ariaLabel="Share Report" onClick={(e) => this.props.onShare(this.props.reportItem)}/>
+                  <IconButton iconProps={{ iconName: 'Cut' }} title="Remove Report" ariaLabel="Remove Report" onClick={(e) => this.props.onRemove(this.props.reportItem)}/>
+                </p>
             </div>
-            
           </div>
-     
-        </div>
+          <div className="row" style={rowStyle}>
+            <div className="col-sm-12">
+              <p className={styles.description}>{reportDesc}</p>
+            </div>
+          </div>
+      </div>
       </div>
     );
   }
