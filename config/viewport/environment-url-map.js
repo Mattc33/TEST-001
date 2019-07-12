7f28765b9b8a7ad742baf21352b224edf7178333
Sanjiv Sharma <sanjivs@slalom.com>
2019-07-12 13:30:57 -0400

    Meeting Book Config

--- config/viewport/environment-url-map.js
+++ config/viewport/environment-url-map.js
@@ -1,4 +1,5 @@
 module.exports = {
     "https://sysco.sharepoint.com/sites/MarketPlaceDev":"sysco-dev",
-    "https://bigapplesharepoint.sharepoint.com/sites/SlalomViewport":"bigapple-dev"
+    "https://bigapplesharepoint.sharepoint.com/sites/SlalomViewportDev":"bigapple-dev",
+    "https://bigapplesharepoint.sharepoint.com/sites/SlalomViewportDev03":"bigapple-dev03",
 };
